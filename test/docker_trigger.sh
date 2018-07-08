3b76937a79ef3cdc8892c9918187c9a842de6790
nullcake <nullcake@github>
2018-07-07 22:46:23 -0500

    update docker_trigger.sh

--- test/docker_trigger.sh
+++ test/docker_trigger.sh
@@ -3,5 +3,5 @@
 TRIGGER_URL="https://registry.hub.docker.com/u/nullcake/graylog-alpine/trigger"
 
 # Trigger all tags/branchs for this automated build.
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST ${TRIGGER_URL}/${DOCKER_TRIGGER_TOKEN} >/dev/null 2>&1
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST ${TRIGGER_URL}/${DOCKER_TRIGGER_TOKEN}/ >/dev/null 2>&1
 
